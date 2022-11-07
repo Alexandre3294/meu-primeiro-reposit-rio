25fed81304f9077890af686dd10bd4f78101dccc
Alexandre Machado <alexandrem16@al.insper.edu.br>
2022-11-07 13:44:01 -0300

    VF

--- sorteia_questao_inedita.py
+++ sorteia_questao_inedita.py
@@ -1,1 +1,9 @@
-print("hello world")
+import random 
+def sorteia_questao(dic,nivel):
+    return random.choice(dic[nivel])
+def sorteia_questao_inedida(dic_quests,nivel, sort):
+    q_resp=sorteia_questao(dic_quests,nivel)
+    while q_resp in sort:
+        q_resp=sorteia_questao(dic_quests,nivel)
+    sort.append(q_resp)
+    return q_resp
