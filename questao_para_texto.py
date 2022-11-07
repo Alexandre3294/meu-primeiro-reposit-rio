f936ba7c06831ae0f69e4ab6fa2b0b948ccb8b2e
Alexandre Machado <alexandrem16@al.insper.edu.br>
2022-11-07 15:27:01 -0300

    alteração '-'

--- questao_para_texto.py
+++ questao_para_texto.py
@@ -1,5 +1,5 @@
 def questao_para_texto(dic, n):
-    resp='QUESTAO {0}\n\n{1}\n\nRESPOSTAS:\n'.format(n, dic['titulo'])
+    resp='{2}\nQUESTAO {0}\n\n{1}\n\nRESPOSTAS:\n'.format(n, dic['titulo'],'-'*40)
     for a,b in dic['opcoes'].items():
         resp+='{0}: {1}\n'.format(a, b)
     return resp
